5baf859c42bdda7e0cf40eccc8a5b05187e1866d
Oleg <oleg.smirnov@exactprosystems.com>
2023-06-26 13:41:46 +0400

    Correct setup.py. Add owasp plugin

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class CustomDist(sdist):
     def run(self):
         copy_tree(f'src/main/proto/{package_name}', package_name)
 
-        copy_tree(f'src/gen/main/services/python/{package_name}', package_name)
+        copy_tree(f'build/generated/source/proto/main/services/python/{package_name}', package_name)
         Path(f'{package_name}/__init__.py').touch()
         Path(f'{package_name}/py.typed').touch()
 
